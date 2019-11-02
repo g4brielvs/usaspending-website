8d9e3564b4358f2836223f9e883082c3bb73f910
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-11-02 17:28:13 -0400

    [story-book] basic tooltip example

--- .storybook/stories/infoTooltip.stories.js
+++ .storybook/stories/infoTooltip.stories.js
@@ -4,7 +4,14 @@ import InfoTooltip from '../components/infoTooltip';
 export default { title: 'Tooltip' };
 
 export const defaultTooltip = () => (
-  <InfoTooltip>
-    <p>This is some tooltipContent</p>
-  </InfoTooltip>
+  <div className="tooltip-story__container">
+    <InfoTooltip>
+      <div>
+        <div className="info-tooltip__title">The title of the Tooltip</div>
+        <div className="info-tooltip__text">
+          <p>Here is some tooltip content that is super neat and informative</p>
+        </div>
+      </div>
+    </InfoTooltip>
+  </div>
 );
