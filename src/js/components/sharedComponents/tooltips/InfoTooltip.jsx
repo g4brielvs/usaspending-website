07b86f1d0807f842267aaea929a05852286fffbe
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-11-18 09:02:44 -0500

    [cl-tooltips] creating reusable variant of tooltip

---
+++ src/js/components/sharedComponents/tooltips/InfoTooltip.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { TooltipWrapper } from 'data-transparency-ui';
+
+const propTypes = {
+    children: PropTypes.node,
+    left: PropTypes.bool,
+    wide: PropTypes.bool,
+    icon: PropTypes.string,
+    controlledProps: PropTypes.shape({
+        isControlled: PropTypes.bool,
+        showTooltip: PropTypes.func,
+        closeTooltip: PropTypes.func,
+        isVisible: PropTypes.bool
+    }),
+    offsetAdjustments: PropTypes.shape({
+        top: PropTypes.number,
+        right: PropTypes.number,
+        left: PropTypes.number
+    }),
+    styles: PropTypes.object
+};
+
+
+export const InfoTooltip = (props) => (
+    <TooltipWrapper tooltipComponent={props.children} {...props} />
+);
+
+InfoTooltip.propTypes = propTypes;
